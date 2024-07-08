ec3974c0af53f155538f35727545acc7fafdd4cc
Tia Nguyen <tia.nkm03@gmail.com>
2024-07-08 10:29:38 +0100

    edit stat

--- src/App.js
+++ src/App.js
@@ -3,49 +3,6 @@ import { useEffect, useState, useId } from "react"
 import LineChart from "./LineChart"
 import PieChart from "./PieChart"
 
-function LocationForm({ location, setLocation, handleSubmit, email, setEmail }) {
-  return (
-    <div className="container">
-      <h2>💡Coding-related activities consume electricity.</h2>
-      <h2>Save the capybaras by choosing the ideal time to push your code!</h2>
-      <form onSubmit={handleSubmit}>
-        <label for="postcode">Enter your regional postcode (i.e. RG41 or SW1)</label>
-        <input
-          type="text"
-          id="postcode"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <label htmlFor="email">And email address if you want a reminder of time to push code</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <button type="submit">Get Carbon Intensity</button>
-      </form>
-    </div>
-  )
-}
-
-function Stat({ stat }) {
-  const generationmix = stat.data[0].generationmix
-
-  return (
-    <div className="container">
-      <h2>
-        Current Carbon Intensity in {stat.shortname} is {stat.data[0].intensity.index}. Push your
-        code!
-      </h2>
-      <PieChart data={generationmix} />
-      <h2>24hrs forecast of Carbon Intensity in {stat.shortname}</h2>
-      <LineChart data={stat.data} />
-
-      {/* {stat.data.map((d) => (
-        <p>
-          {d.intensity.forecast} {d.intensity.index}
-        </p>
-      ))} */}
-    </div>
-  )
-}
-
 export default function App() {
   const [location, setLocation] = useState("")
   const [email, setEmail] = useState("")
@@ -121,6 +78,53 @@ export default function App() {
         setEmail={setEmail}
       />
       {stat && <Stat location={location} stat={stat} />}
+    </div>
+  )
+}
+
+function LocationForm({ location, setLocation, handleSubmit, email, setEmail }) {
+  return (
+    <div className="container">
+      <h2>💡Coding-related activities consume electricity.</h2>
+      <h2>Save the capybaras by choosing the ideal time to push your code!</h2>
+      <form onSubmit={handleSubmit}>
+        <label for="postcode">Enter your regional postcode (i.e. RG41 or SW1)</label>
+        <input
+          type="text"
+          id="postcode"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <label htmlFor="email">And email address if you want a reminder of time to push code</label>
+        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit">Get Carbon Intensity</button>
+      </form>
+    </div>
+  )
+}
+
+function Stat({ stat }) {
+  const generationmix = stat.data[0].generationmix
+
+  return (
+    <div className="container">
+      <h2>
+        Current Carbon Intensity in {stat.shortname} is {stat.data[0].intensity.index}.
+      </h2>
+      <h2>
+        {stat.data[0].intensity.index === ("low" || "very low")
+          ? " Push your code!"
+          : " Now it's not the best time. Come back later or we'll send you a notification."}
+      </h2>
+      <PieChart data={generationmix} />
+      <h2>24hrs forecast of Carbon Intensity in {stat.shortname}</h2>
+      <LineChart data={stat.data} />
+
+      {/* {stat.data.map((d) => (
+        <p>
+          {d.intensity.forecast} {d.intensity.index}
+        </p>
+      ))} */}
     </div>
   )
 }
